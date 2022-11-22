dcde9c2b20c6f512df404351008faed371ab2b13
Ruslan <r8446101@gmail.com>
2022-11-22 22:37:49 +0300

    Task 2 solution

--- DZ_8.py
+++ DZ_8.py
@@ -2,3 +2,21 @@ def camel_case(string: str) -> str:
     string_title = string.title()
     return print(string_title.replace(" ", ""))
 
+def palindromeDZ(num: int) -> int:
+ i = num + 1
+ while str(i)[0] != str(i)[-1]:
+    i += 1
+ return print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
