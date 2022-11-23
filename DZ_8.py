8def4ff5256013929265f8d26eb13f63f6029ec8
Ruslan <r8446101@gmail.com>
2022-11-23 21:53:44 +0300

    DZ_8

---
+++ DZ_8.py
@@ -0,0 +1,21 @@
+def camel_case(string: str) -> str:
+    string_title = string.title()
+    return print(string_title.replace(" ", ""))
+
+def palindromeD_Z(num: int) -> int:
+ i = num + 1
+ while str(i)[0] != str(i)[-1]:
+    i += 1
+ return print(i)
+
+
+
+
+
+
+
+
+
+
+
+
