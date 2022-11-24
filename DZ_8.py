f5a8053a435ef81d7957abb7636060f5b83526d8
Ruslan <r8446101@gmail.com>
2022-11-24 23:17:23 +0300

    Solution of 3 tasks

--- DZ_8.py
+++ DZ_8.py
@@ -1,6 +1,9 @@
 def camel_case(string: str) -> str:
-    string_title = string.title()[0:-1]
-    return print(string_title.replace("-", ""))
+    string_title = string.title()[1:-1]
+    if len(string) == 0:
+        return "Error"
+    return print(string[0] + string_title.replace("-", "").replace("_", ""))
+
 
 
 def palindromeD_Z(num: int) -> int:
@@ -8,10 +11,20 @@ def palindromeD_Z(num: int) -> int:
  if num <= 999:
   while str(i)[0] != str(i)[-1]:
       i += 1
- elif num >=1000:
+ elif num >= 1000:
    while str(i)[0] != str(i)[-1] or str(i)[1] != str(i)[2]:
       i += 1
  return print(i)
+
+def get_digit(word: str) -> int or None:
+    for symbol in word:
+        if symbol.isdigit():
+            return int(symbol)
+    return None
+def order(array: str) -> str:
+    return " ".join(sorted(array.split(), key=get_digit))
+
+
 
 
 
