f2d875e55396ecff8f95b8eb3a2d10f1760d661d
Ruslan <r8446101@gmail.com>
2022-11-24 19:03:10 +0300

    Adjustment DZ_8

--- DZ_8.py
+++ DZ_8.py
@@ -1,12 +1,18 @@
 def camel_case(string: str) -> str:
-    string_title = string.title()
-    return print(string_title.replace(" ", ""))
+    string_title = string.title()[0:-1]
+    return print(string_title.replace("-", ""))
+
 
 def palindromeD_Z(num: int) -> int:
  i = num + 1
- while str(i)[0] != str(i)[-1]:
-    i += 1
+ if num <= 999:
+  while str(i)[0] != str(i)[-1]:
+      i += 1
+ elif num >=1000:
+   while str(i)[0] != str(i)[-1] or str(i)[1] != str(i)[2]:
+      i += 1
  return print(i)
+
 
 
 
