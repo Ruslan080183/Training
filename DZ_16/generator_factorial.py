9db5f159a54e9091d2c6da732d07d21c61633827
Ruslan <r8446101@gmail.com>
2023-01-03 19:46:15 +0300

    DZ_16(reduce,fibonacci new)

--- DZ_16/generator_factorial.py
+++ DZ_16/generator_factorial.py
@@ -1,7 +1,15 @@
+from functools import reduce
 def factorial_generator(number: int) -> int:
     factorial = 1
     while number > 1:
         factorial *= number
         number -= 1
         yield factorial
+
+def factorial_generator_reduce(number1: int) -> int:
+    factorial1 = reduce(lambda x, y: x * y, range(1, number1 + 1))
+    return factorial1
+
+
+
 
