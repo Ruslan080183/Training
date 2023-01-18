3719ba60ad3e99d18de2068d8043c22825141cd3
Ruslan <r8446101@gmail.com>
2023-01-18 21:42:12 +0300

    Create model RatingViews

--- DZ_181/project/app1/admin.py
+++ DZ_181/project/app1/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Genre, Film, Viewer
+from .models import Genre, Film, Viewer, RatingViews
 
 admin.site.register(Genre)
 admin.site.register(Film)
-admin.site.register(Viewer)
+admin.site.register(RatingViews)
