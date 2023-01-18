41827bfe48bb5321fe7bcce95a38ca1327049a58
Ruslan <r8446101@gmail.com>
2023-01-18 20:58:03 +0300

    Create model Film

--- DZ_181/project/app1/admin.py
+++ DZ_181/project/app1/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Genre
+from .models import Genre, Film
 
 admin.site.register(Genre)
+admin.site.register(Film)
--- DZ_181/project/app1/models.py
+++ DZ_181/project/app1/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 class Genre(models.Model):
+
     title = models.CharField(max_length=200)
     def __str__(self):
         return self.title
+class Film(models.Model):
 
+    title = models.CharField(max_length=300)
+    country = models.CharField(max_length=200)
+    year = models.IntegerField(max_length=100)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return F"{self.title} {self.country} {self.year}"
 
